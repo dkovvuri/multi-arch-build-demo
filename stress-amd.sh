5c67a794eed3d2c4761186133018a9c8d3d9e5f5
Deepak Kovvuri <45470146+dkovvuri@users.noreply.github.com>
2023-06-28 14:52:31 -0400

    Update stress-amd.sh

--- stress-amd.sh
+++ stress-amd.sh
@@ -2,21 +2,14 @@
 
 AMD_SERVICE=$(aws cloudformation describe-stacks --stack-name ecs-task-demo --query 'Stacks[0].Outputs[?OutputKey==`AMDService`].OutputValue' --output text --region us-east-1)
 
-# Define the function to invoke the stress test endpoint
-invoke_stress_test() {
-  while true; do
-    curl -sS "http://$AMD_SERVICE/stress?number=99999999999999"
-    sleep 0.1  # Adjust the sleep duration as needed
-  done
-}
+# Set the URL of the web service to test
+URL="http://$AMD_SERVICE/stress?number=99999999999999"
 
-# Set the number of simultaneous sessions
-num_sessions=20
+# Set the number of requests to be made
+NUM_REQUESTS=10000
 
-# Run the stress test in the background for each session
-for ((i=1; i<=num_sessions; i++)); do
-  invoke_stress_test &
-done
+# Set the number of concurrent requests to be made
+CONCURRENCY=10
 
-# Wait for all background processes to finish
-wait
+# Perform the load test
+ab -n $NUM_REQUESTS -c $CONCURRENCY $URL
