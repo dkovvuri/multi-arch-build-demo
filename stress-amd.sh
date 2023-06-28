20b0ebe34a17e2fd6f2ec9bb40039f2788c6953a
Deepak Kovvuri <45470146+dkovvuri@users.noreply.github.com>
2023-06-28 15:00:11 -0400

    Update stress-amd.sh

--- stress-amd.sh
+++ stress-amd.sh
@@ -6,10 +6,21 @@ AMD_SERVICE=$(aws cloudformation describe-stacks --stack-name ecs-task-demo --qu
 URL="http://$AMD_SERVICE/stress?number=99999999999999"
 
 # Set the number of requests to be made
-NUM_REQUESTS=10000
+NUM_REQUESTS=10000000
 
 # Set the number of concurrent requests to be made
 CONCURRENCY=10
 
 # Perform the load test
-ab -n $NUM_REQUESTS -c $CONCURRENCY $URL
+for ((i=1; i<=$CONCURRENCY; i++))
+do
+  {
+    for ((j=1; j<=$NUM_REQUESTS/$CONCURRENCY; j++))
+    do
+      curl -s $URL
+    done
+  } &
+done
+
+# Wait for all background processes to finish
+wait
